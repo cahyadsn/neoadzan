5c6233754d5d6ccd9aca3136ddc9bdaca25d0f1f
cahya dsn <cahyadsn@gmail.com>
2025-03-01 10:05:31 +0700

    update 2503011005

--- inc/db.php
+++ inc/db.php
@@ -6,7 +6,7 @@ FILENAME     : db.php
 PURPOSE      : db connection configuraton
 AUTHOR       : CAHYA DSN
 CREATED DATE : 2018-01-25
-UPDATED DATE : 2021-03-07
+UPDATED DATE : 2025-03-01 08:47:07
 DEMO SITE    : http://neoadzan.cahyadsn.com
 SOURCE CODE  : https://github.com/cahyadsn/neoadzan
 ================================================================================
@@ -23,13 +23,13 @@ SOFTWARE.
 
 See the MIT License for more details
 
-copyright (c) 2018-2021 by cahya dsn; cahyadsn@gmail.com
+copyright (c) 2018-2025 by cahya dsn; cahyadsn@gmail.com
 ================================================================================*/
 $dbhost='localhost';
 $dbuser='root';
 $dbpass='';
-$dbname='db_wilayah';
-$dbtable='wilayah_137_v1';
+$dbname='wilayah';
+$dbtable='wilayah_level_1_2';
 $db_dsn = "mysql:dbname=$dbname;host=$dbhost";
 try {
   $db = new PDO($db_dsn, $dbuser, $dbpass);
